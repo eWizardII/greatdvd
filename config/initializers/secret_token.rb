f843a986330969ac518950d4c1923d28e67ec220
sabiola <sabiola@ubuntu.ubuntu-domain>
2012-02-11 02:42:59 -0500

    vanilla rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thegreatdvd::Application.config.secret_token = 'cf4137ef6764ab1c5c80ed501fd192db9865b09b9922326c9d66984a65f2a01153bcfd704f246927f0c4ffb39e0a5c6a832ad9f7b6c974133ccaa6737aba5355'
