b7aea297598ce7891611eb14f765446156a186a8
sabiola <sabiola@ubuntu.ubuntu-domain>
2012-02-11 19:15:59 -0500

    added search information to the controller and model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,12 +2,13 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movies.search(params[:search])
+    # @movies = Movie.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @movies }
-    end
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.json { render :json => @movies }
+    # end
   end
 
   # GET /movies/1
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+	def self.search(search)
+		if search
+			find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+		else
+			find(:all)
+		end
 end
