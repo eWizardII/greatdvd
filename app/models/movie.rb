663c401e16e47b7b79cf848ff6a9cb10899db06e
sabiola <sabiola@ubuntu.ubuntu-domain>
2012-02-11 19:25:46 -0500

    database problem with naming should be title and not name - may be case sensitive if this push doesn't work

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
 	def self.search(search)
 		if search
-			find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+			find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
 		else
 			find(:all)
 		end
