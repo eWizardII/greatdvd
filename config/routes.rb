01e5a4e104ba11bb28db06b85dd4d3f62bf8bbfc
sabiola <sabiola@ubuntu.ubuntu-domain>
2012-02-20 01:39:35 -0500

    fixed all the links on the site

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Thegreatdvd::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'movies#index'
 
   # See how all your routes lay out with "rake routes"
 
