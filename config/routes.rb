c1528a1266ede2c67463a9acdc8e8a16e23ea57a
sabiola <sabiola@ubuntu.ubuntu-domain>
2012-02-20 20:02:33 -0500

    foo

--- config/routes.rb
+++ config/routes.rb
@@ -50,8 +50,8 @@ Thegreatdvd::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'movies#index'
-
+  #root :to => 'movies#index'
+  map.root :controller => 'movies'
   # Should route to the page with the movies in Heroku
 
   # See how all your routes lay out with "rake routes"
