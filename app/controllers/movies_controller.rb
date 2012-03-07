535fd4db998be9359d822ecb4cd8c5f30ce9d611
sabiola <sabiola@ubuntu.ubuntu-domain>
2012-03-07 07:51:04 -0500

    fixed a bunch of issues, infinite loop, form validation errors, cookies craziness, etc.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,14 @@
 class MoviesController < ApplicationController
   def index
+
+  	if cookies[:uid].nil? then
+  		cookies.permanent.signed[:present] = "no"
+  		cookies.permanent.signed[:uid] = "1746"
+  		redirect_to "http://www.google.com"
+  	else
+		cookies.permanent.signed[:present] = "yes"
+	end
+
     if params[:start].present? then
       @movies = Movie.search(params[:search], params[:start])
     else
